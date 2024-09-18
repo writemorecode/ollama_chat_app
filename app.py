0301c7c3a6e821b5a1cdddcda56de5e230a90f4f
writemorecode <gustav.ae.karlsson@gmail.com>
2024-09-18 14:04:55 +0200

    Added some type hints

--- app.py
+++ app.py
@@ -1,47 +1,38 @@
+import base64
+from typing import Generator
+
+import ollama
 from flask import (
     Flask,
     Response,
     render_template,
     stream_with_context,
 )
-import ollama
-import base64
-import logging
 
 app = Flask(__name__)
-
 MODEL = "gemma2:2b"
-
 client = ollama.Client(host="http://ollama:11434")
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.StreamHandler()  # Output logs to console
-    ],
-)
 
 
 @app.route("/")
-def index():
+def index() -> str:
     return render_template("index.html")
 
 
 @app.route("/stream/<prompt_b64>")
-def stream(prompt_b64: str):
+def stream(prompt_b64: str) -> Response:
     prompt = base64.b64decode(prompt_b64).decode()
 
-    def generate():
+    def generate() -> Generator[str]:
         stream = client.chat(
             model=MODEL,
+            stream=True,
             messages=[
                 {
                     "role": "user",
                     "content": prompt,
                 },
             ],
-            stream=True,
         )
         for chunk in stream:
             yield f"data: {chunk['message']['content']}\n\n"
@@ -50,4 +41,4 @@ def stream(prompt_b64: str):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000 , debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
